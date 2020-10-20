fc1ead5284f6c96868b41f579e72f20a00111b6b
Erik Blidner <erik.blidner@gmail.com>
2020-10-20 21:07:34 +0200

    added rules method

--- GuessingGame.java
+++ GuessingGame.java
@@ -2,7 +2,24 @@ public class GuessingGame {
 
     public static void main(String[] args) {
     
-    Guesser Guess = new Guesser(0, 999);
+    Guesser Range = new Guesser(0, 1000);
     
  }
+
+   public void start() {
+       Range.rules();
+       Range.doGuesses();
+    // call the rules method here
+    // call the doGuesses() method here
+  }
+
+
+  private void rules(){
+    System.out.println("Think of a number between " +
+                       low + " and "  + high);
+    System.out.println("I'm going to ask a few questions in order " +
+                       "to guess the number.");
+    System.out.println("Please answer T for true, and F for false.\n");
+  }
+
 }
